c15d25806c88b5e33f3c70c1457a62a4ac83797e
AlexAdamov <alx.adamov@gmail.com>
2024-08-20 07:54:50 +0200

    port definition

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: import.meta.env.PORT, // Specify your desired port here
+  },
 })
