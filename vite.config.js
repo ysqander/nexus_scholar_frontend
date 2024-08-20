724d0af75c65cf911530b563eb0dc18b9e3ae6ed
AlexAdamov <alx.adamov@gmail.com>
2024-08-20 16:07:36 +0200

    trying Caddyfile config files

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
-    server: {
-      port: parseInt(env.PORT),
-    },
+    // server: {
+      // port: parseInt(env.PORT),
+    // },
   }
 })
