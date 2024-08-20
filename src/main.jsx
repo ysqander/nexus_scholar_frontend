57fbec2770c3d208627095399651eec33f62cf63
AlexAdamov <alx.adamov@gmail.com>
2024-08-20 15:30:03 +0200

    debug Auth0 env variables

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
+// Log the environment variables
+console.log('Auth0 Domain:', import.meta.env.VITE_AUTH0_DOMAIN)
+console.log('Auth0 Client ID:', import.meta.env.VITE_AUTH0_CLIENT_ID)
+console.log('Auth0 Audience:', import.meta.env.VITE_AUTH0_AUDIENCE)
+console.log('Redirect URI:', window.location.origin)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       }}
     >
       <Router>
